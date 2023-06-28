3f722b8bf0360f86936825764f25e304be206f3d
MariSalcedo <123317410+MariSalcedo@users.noreply.github.com>
2023-06-28 09:09:40 -0600

    :)

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -7,10 +7,35 @@ public class CameraFollow : MonoBehaviour
     public Transform quienEsElJugador;
     public float offset = 0f;
 
+    public Vector2 minLim;
+    public Vector2 maxLim;
+
     // Update is called once per frame
     void Update()
     {
+        float targetX = quienEsElJugador.position.x + offset;
+        float targetY = quienEsElJugador.position.y + offset;
+
+        if (targetX < minLim.x)
+        {
+            targetX = minLim.x;
+        }
+        else if (targetX > maxLim.x)
+        {
+            targetX = maxLim.x;
+        }
+        if (targetY < minLim.y)
+        {
+            targetY = minLim.y;
+        }
+        else if (targetY > maxLim.y)
+        {
+            targetY = maxLim.y;
+        }
+
+        transform.position = new Vector3(targetX, targetY, transform.position.z);
+
         //Nombre de la variable
-        transform.position = new Vector3(quienEsElJugador.position.x + offset, quienEsElJugador.position.y + offset, transform.position.z);
+        //transform.position = new Vector3(quienEsElJugador.position.x + offset, quienEsElJugador.position.y + offset, transform.position.z);
     }
 }
