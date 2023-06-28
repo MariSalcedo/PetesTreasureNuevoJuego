d57b32c2eedf92c8e50547ee9c557d4652fdf82d
MariSalcedo <123317410+MariSalcedo@users.noreply.github.com>
2023-06-28 10:54:38 -0600

    .

--- Assets/Scripts/Patrol.cs
+++ Assets/Scripts/Patrol.cs
@@ -4,15 +4,32 @@ using UnityEngine;
 
 public class Patrol : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public GameObject objetoMover;
+    public float vel;
+    public Transform puntoActual;
+    public Transform[] puntos;
+    public int puntoSeleccionado;
+
     void Start()
     {
-        
+        puntoActual = puntos[puntoSeleccionado];
     }
 
-    // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        //Código para hacer que el enemigo se mueva de lado a lado:
+        //La posición del objeto a mover va a estar cambiando. Hacia dónde se va a mover, va entre los paréntesis.
+        objetoMover.transform.position = Vector3.MoveTowards(objetoMover.transform.position, puntoActual.position, Time.deltaTime * vel);
+
+        if (objetoMover.transform.position == puntoActual.position)
+        {
+            puntoSeleccionado += 1;
+            //Longitud de un Array determinado.
+            if(puntoSeleccionado == puntos.Length)
+            {
+                puntoSeleccionado = 0;
+            }
+            puntoActual = puntos[puntoSeleccionado];
+        }
     }
 }
